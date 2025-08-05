8e04836f93fb7b445a6fb7e2ef91591f2e347895
Riya Solanki <riyahsolanki@gmail.com>
2025-08-04 21:52:34 -0500

    2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,24 +2,27 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
+
+  const names = persons.map((person) => {return person.name})
 
   const handleSetPersons = (event) => {
     event.preventDefault()
     console.log(event.target.value)
 
-    const names = persons.map((person) => {return person.name})
-
     if (names.includes(newName) === true) {
       alert(`${newName} is already in the phonebook.`)
     }
     else {
-      setPersons(persons.concat({name: newName, phone: newPhone}))
+      setPersons(persons.concat({name: newName, number: newNumber}))
     }
-    
+
     setNewName('')
-    setNewPhone('')
+    setNewNumber('')
   }
   
   const [newName, setNewName] = useState('')
@@ -28,29 +31,46 @@ const App = () => {
     setNewName(event.target.value)
   }
 
-  const [newPhone, setNewPhone] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const handleSetNewPhone = (event) => {
-    setNewPhone(event.target.value)
+  const handleSetNewNumber = (event) => {
+    setNewNumber(event.target.value)
   }
+
+  const [nameFilter, setNameFilter] = useState('')
+
+  const handleSetNameFilter = (event) => {
+    setNameFilter(event.target.value)
+  }
+
+  const filteredPersons = persons.filter(person => {
+    const str1 = person.name.toLowerCase()
+    const str2 = nameFilter.toLowerCase()
+
+    return str1.includes(str2)
+  })
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input id='filterField' value={nameFilter} onChange={handleSetNameFilter}></input>
+      </div>
+      <h2>Add New</h2>
       <form onSubmit={handleSetPersons}>
         <div>
           name: <input id='nameField' value={newName} onChange={handleSetNewName}/>
         </div>
         <div>
-          number: <input id='phoneField' value={newPhone} onChange={(handleSetNewPhone)}/>
+          number: <input id='phoneField' value={newNumber} onChange={(handleSetNewNumber)}/>
         </div>
         <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) =>
-        <p key={person.name}>{person.name} {person.phone}</p>
+      {filteredPersons.map((person) =>
+        <p key={person.id}>{person.name} {person.number}</p>
       )}
     </div>
   )
