792fb7ed91a97a41886892da175ded9ce859190d
Riya Solanki <riyahsolanki@gmail.com>
2025-08-24 23:40:38 -0500

    add country filter

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,13 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import countryService from './services/countries'
 
 function App() {
+  const [countryFilter, setCountryFilter] = useState('')
+  const [countryNames, setCountryNames] = useState([])
 
-  const countryFilterString = useState('')
+  const filteredCountries = countryNames.filter((name) => {
+    return name.toLowerCase().includes(countryFilter.toLowerCase())
+  })
+
+  function getDataHook() {
+    countryService
+      .getAllCountries()
+      .then((response) => {
+        setCountryNames(response.data.map((country) => country.name.common))
+      })
+  }
+
+  function handleSetCountryFilter(event) {
+    setCountryFilter(event.target.value)
+    console.log(filteredCountries)
+  }
+
+  useEffect(getDataHook, [])
 
   return (
     <>
       <div className='m-2'>
-        find countries: <input id='countryFilter'/>
+        find countries: 
+        <input 
+          id='countryFilter'
+          onChange={handleSetCountryFilter}
+          value={countryFilter}/>
       </div>
     </>
   )
--- part2/countries/src/services/countries.js
+++ part2/countries/src/services/countries.js
@@ -1,1 +1,9 @@
 import axios from 'axios'
+
+const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api'
+
+function getAllCountries() {
+  return axios.get(`${baseUrl}/all`)
+}
+
+export default { getAllCountries }
